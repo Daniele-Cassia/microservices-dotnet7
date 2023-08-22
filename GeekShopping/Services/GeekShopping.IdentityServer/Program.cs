348b45c3946e586af4bac0e5471415b2a6232086
Daniele Cassia <danielecassia9@gmail.com>
2023-08-22 14:51:57 -0300

    configuração do program.cs

--- GeekShopping/Services/GeekShopping.IdentityServer/Program.cs
+++ GeekShopping/Services/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,45 @@
+using GeekShopping.IdentityServer.Configuration;
+// using GeekShopping.IdentityServer.Initializer;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    {
+        var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+        options.UseMySql(
+            connection,
+            new MySqlServerVersion(new Version(8, 0, 34)));
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var IdentityServerBuilder = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+
+})
+.AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+// builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+IdentityServerBuilder.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -9,16 +47,25 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseIdentityServer();
+
 app.UseAuthorization();
+
+// Initialize User Database
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+//     dbInitializer.Initialize();
+// }
+
 
 app.MapControllerRoute(
     name: "default",
